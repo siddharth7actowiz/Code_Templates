433e9f0005d31f07ee34aad3e29efc7f4b42d031
siddharth7actowiz <siddharth7.actowiz@gmail.com>
2026-03-11 22:42:55 +0530

    Update and rename unzip_and_load_json.py to utills.py.py

--- utills.py.py
+++ utills.py.py
@@ -18,3 +18,25 @@ def load_json(input_src_folder):
 
     return json_data
 
+import os
+import gzip
+import json
+from config import *
+
+def load_files(data_dir,start_val,end_val):
+
+    files = os.listdir(data_dir)
+    files.sort()
+
+    for file in files[start_val:end_val]:
+
+        path = os.path.join(data_dir, file)
+
+        try:
+
+            with gzip.open(path, "rt", encoding="utf-8") as f:
+                yield json.load(f)
+
+        except Exception as e:
+
+            print("Failed file:", file, e)
