997eeec9a94963f7fb53cf2a76f304d51cc9abca
siddharth7actowiz <siddharth7.actowiz@gmail.com>
2026-03-11 22:55:19 +0530

    db.py

--- MYSQL_Databse_PDBC_Temp.py
+++ MYSQL_Databse_PDBC_Temp.py
@@ -2,200 +2,86 @@ import json
 import mysql.connector
 from typing import List, Tuple
 from datetime import datetime
-
-
-# ==========================================
-# CONFIGURATION
-# ==========================================
-
-DB_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "sid",
-    "database": "grabfood",
-    "port": 3306
-}
-
-BATCH_SIZE = 200
-
-
+from config import DB_CONFIG
 # ==========================================
 # DATABASE CONNECTION
 # ==========================================
 
 def get_connection():
-    con = mysql.connector.connect(**DB_CONFIG)
-    con.autocommit = False
-    return con
+    try:
+        con = mysql.connector.connect(**DB_CONFIG)
+        con.autocommit = False
+        return con
+    except Execption as e:
+        print("Error in Db Connection",get_connection.__name__,e)
 
+def create_table(Table_N,cursor):
+    ddl=f'''
+            CREATE TABLE IF NOT EXISTS {table_name}();
+            
+        '''
+    cursor.execute(create_query)
+
+            
+    
 
 # ==========================================
 # BATCH INSERT FUNCTION
 # ==========================================
-
-def batch_insert(cursor, insert_query: str, values: List[Tuple], batch_size: int = BATCH_SIZE):
+def batch_insert(
+    cursor,
+    con,
+    insert_query: str,
+    values: List[Tuple],
+    batch_size: int = db_batches,
+) -> Tuple[int, list]:
 
     total_records = len(values)
     batch_count = 0
     failed_batches = []
 
-    for start in range(0, total_records, batch_size):
-        end = min(start + batch_size, total_records) # to avoid array out of bound index error
-        batch = values[start:end]
+    for s in range(0, total_records, batch_size):
+        e = min(s + batch_size, total_records)
+        batch = values[s:e]
 
         try:
             cursor.executemany(insert_query, batch)
             con.commit()
             batch_count += 1
-            print(f"Inserted batch {batch_count} ({start} → {end})")
 
-        except Exception as e:
-            print(f"Batch failed ({start} → {end})")
-            print("Error:", e)
+        except Exception as exp:
+            print(f"[BATCH FAILED] rows {s} to {e}  {exp}")
             failed_batches.append(batch)
 
     return batch_count, failed_batches
 
 
-# ==========================================
-# DATA TRANSFORMATION FUNCTION
-# ==========================================
 
-def transform_json_to_db_values(json_data):
-
-    restaurant_values = []
-    menu_values = []
-
-    for data in json_data:
-        rest = data.get("Restaurant_Details", {})
-        menu_items = data.get("Menu_Items", [])
-
-        restaurant_values.append((
-            rest.get("Restaurant_ID"),
-            rest.get("Restaurant_Name"),
-            rest.get("Branch_Name"),
-            rest.get("Cuisine"),
-            json.dumps(rest.get("Tip")),
-            rest.get("Timezone"),
-            rest.get("ETA"),
-            json.dumps(rest.get("DeliveryOptions")),
-            rest.get("Rating"),
-            rest.get("Is_Open"),
-            rest.get("Currency_Code"),
-            rest.get("Currency_Symbol"),
-            json.dumps(rest.get("Offers")),
-            rest.get("Timing_Everyday")
-        ))
-
-        for item in menu_items:
-            menu_values.append((
-                item.get("Restaurant_ID"),
-                item.get("Category_Name"),
-                item.get("Item_ID"),
-                item.get("Item_Name"),
-                item.get("Item_Description"),
-                item.get("Item_Price"),
-                item.get("Item_Discounted_Price"),
-                json.dumps(item.get("Item_Image_URL")),
-                json.dumps(item.get("Item_Thumbnail_URL")),
-                item.get("Item_Available"),
-                item.get("Is_Top_Seller"),
-            ))
-
-    return restaurant_values, menu_values
-
-
-# ==========================================
-# MAIN EXECUTION FUNCTION
-# ==========================================
-
-def run_ingestion(json_file_path):
-
-    start_time = datetime.now()
-
-    # Load JSON
-    with open(json_file_path, "r", encoding="utf-8") as f:
-        json_data = json.load(f)
-
-    # Transform
-    restaurant_values, menu_values = transform_json_to_db_values(json_data)
-
-    # Queries
-    insert_rest_query = """
-    INSERT INTO RESTAURANT_TABLE (
-        Restaurant_ID,
-        Restaurant_Name,
-        Branch_Name,
-        Cuisine,
-        Tip,
-        Timezone,
-        ETA,
-        DeliveryOptions,
-        Rating,
-        Is_Open,
-        Currency_Code,
-        Currency_Symbol,
-        Offers,
-        Timing_Everyday
-    )
-    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-    """
-
-    insert_menu_query = """
-    INSERT INTO MENU_ITEMS_TABLE (
-        Restaurant_ID,
-        Category_Name,
-        Item_Id,
-        Item_Name,
-        Item_Description,
-        Item_Price,
-        Item_Discounted_Price,
-        Item_Image_URL,
-        Item_Thumbnail_URL,
-        Item_Available,
-        Is_Top_Seller
-    )
-    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-    """
-
-    con = get_connection()
-    cursor = con.cursor()
-
+def insert_into_db(cursor, con, parsed_batch: List[dict], TABLE_NAME: str):
     try:
-        print("Starting Restaurant Batch Insert...")
-        rest_batches, rest_failed = batch_insert(
-            cursor, insert_rest_query, restaurant_values
-        )
+        if not parsed_batch:
+            return
 
-        print("Starting Menu Batch Insert...")
-        menu_batches, menu_failed = batch_insert(
-            cursor, insert_menu_query, menu_values
-        )
+        cols = ", ".join(parsed_batch[0].keys())
+        placeholders = ", ".join(["%s"] * len(parsed_batch[0]))
 
-        
-        print("Transaction Successful")
-        print(f"Restaurant batches: {rest_batches}")
-        print(f"Menu batches: {menu_batches}")
+        insert_query = f"""
+            INSERT INTO {TABLE_NAME} ({cols})
+            VALUES ({placeholders})
+            ON DUPLICATE KEY UPDATE col = col;
+        """
 
-        print("Failed Restaurant Rows:", sum(len(b) for b in rest_failed))
-        print("Failed Menu Rows:", sum(len(b) for b in menu_failed))
+        values = [tuple(row.values()) for row in parsed_batch]
+
+       
+        for row_values in values:
+            formatted_vals = ", ".join(
+                ("'" + str(v).replace("'", "''") + "'") if isinstance(v, str) else "NULL" if v is None else str(v)
+                for v in row_values
+            )
+            print(f"INSERT INTO {TABLE_NAME} ({cols}) VALUES ({formatted_vals}) ON DUPLICATE KEY UPDATE Res_Id = Res_Id;")
+
+        batch_insert(cursor, con, insert_query, values)
 
     except Exception as e:
-        con.rollback()
-        print("Transaction Failed")
-        print("Error:", e)
-
-    finally:
-        cursor.close()
-        con.close()
-
-    end_time = datetime.now()
-    print("Execution Time:", end_time - start_time)
-
-
-# ==========================================
-# ENTRY POINT
-# ==========================================
-
-if __name__ == "__main__":
-    run_ingestion("GRABFOOD_MENU_2026_02_26.json")
+        print(f"Error in insert_into_db: {e}")
